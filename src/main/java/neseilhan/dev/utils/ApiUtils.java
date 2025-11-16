dc7a181a0970c7408a3724f9b37ccc59ed59f8be
LENOVO <neseilhannn@gmail.com>
2025-11-17 00:53:34 +0300

    api utils

--- src/main/java/neseilhan/dev/utils/ApiUtils.java
+++ src/main/java/neseilhan/dev/utils/ApiUtils.java
@@ -1,10 +1,11 @@
-package org.example.utils;
+package neseilhan.dev.utils;
+
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.example.config.ConfigManager;
+import neseilhan.dev.config.ConfigManager;
 
 public class ApiUtils {
     private static final ConfigManager config = ConfigManager.getInstance();
@@ -13,25 +14,33 @@ public class ApiUtils {
         RestAssured.baseURI = config.getBaseUrl();
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
-
+    /**
+     * JSON içerik tipi ile temel bir RestAssured isteği oluşturur.
+     */
     public static RequestSpecification getBaseRequest() {
         return RestAssured.given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON);
     }
-
+    /**
+     * API anahtarı (key) ve token eklenmiş, doğrulamalı bir istek döner.
+     */
     public static RequestSpecification getAuthenticatedRequest() {
         return getBaseRequest()
                 .queryParam("key", config.getApiKey())
                 .queryParam("token", config.getApiToken());
     }
-
+    /**
+     * API yanıtını loglayarak konsola durum kodu, süre ve body bilgisi yazar.
+     */
     public static void logResponse(Response response) {
         System.out.println("Status Code: " + response.getStatusCode());
         System.out.println("Response Time: " + response.getTime() + "ms");
         System.out.println("Response Body: " + response.getBody().asString());
     }
-
+    /**
+     * API yanıtının beklenen durum kodunu döndürdüğünü doğrular.
+     */
     public static void validateStatusCode(Response response, int expectedStatusCode) {
         if (response.getStatusCode() != expectedStatusCode) {
             throw new AssertionError(
@@ -41,5 +50,5 @@ public class ApiUtils {
         }
     }
 
-    }
+}
 
